30cdbdf38340cbd83044bc6d64ac5501ccfe3a33
adnan-2619 <zayedadnan1901@gmail.com>
2025-11-16 00:50:29 +0600

    resolve all the elements

--- script.js
+++ script.js
@@ -8,22 +8,8 @@ menu.onclick = () =>{
 
 }
 
-window.onscroll = () =>{
-
-  menu.classList.remove('fa-times');
-  navbar.classList.remove('active');
-
-  if(window.scrollY > 60)
-    {
-    document.querySelector('#scroll-top').classList.add('active');
-  }
-  else{
-    document.querySelector('#scroll-top').classList.remove('active');
-  }
-
-}
-
-function loader(){
+function loader()
+{
   document.querySelector('.loader-container').classList.add('fade-out');
 }
 
